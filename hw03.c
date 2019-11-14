7b9f3145c19b9fba4d5bbfc047baebcec1b36812
Alex Faust <alexander.faust@cooper.edu>
2019-11-13 19:22:00 -0500

    Finished Homework

--- hw03.c
+++ hw03.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef enum {
   NAME_ONLY = 0,
@@ -9,7 +10,8 @@ typedef enum {
 typedef struct {
   char *first_name;
   char *last_name;
-  // TODO: add fields here for major and year
+  char *major;
+  int year;
 } Student;
 
 void print_student(Mode m, Student s) {
@@ -17,27 +19,28 @@ void print_student(Mode m, Student s) {
     case NAME_ONLY:
       printf("%s %s\n", s.first_name, s.last_name);
       break;
-  // TODO: handle other cases
+	case MAJOR_AND_NAME:
+	  printf("%s %s %s\n", s.major, s.first_name, s.last_name);
+	  break;
+	case YEAR_AND_NAME:
+	  printf("%d %s %s\n", s.year, s.first_name, s.last_name);
+	  break;
+	default:
+	  break;
   }
 }
 
-/*
-argv[1] will be the Mode integer.
-argv[2]-argv[5] will be student info in the form [first_name, last_name, major, year].
-argv[6]-argv[9], if present will be student info for a second student in the same form, and so on.
-You will need to check argc to determine the number of students passed at runtime.
-The fields are all strings (char[]), except year is a number (int)
-*/
 int main(int argc, char **argv) {
-  // TODO: parse argv to populate student structs 
-  // for now, here's two hardcoded students:
-  Student s1, s2;
-  s1.first_name = "Julia";
-  s1.last_name = "A";
-  s2.first_name = "Peter";
-  s2.last_name = "Cooper";
-  
-  print_student(NAME_ONLY, s1);
-  print_student(NAME_ONLY, s2);
+  Mode format = atoi(argv[1]);
+  int numStudents = (argc-2)/4;
+  char **studentList = argv+2;
+  Student student;
+  for(int i = 0; i < numStudents; i++) {
+		student.first_name = studentList[4*i];
+		student.last_name = studentList[4*i + 1];
+		student.major = studentList[4*i + 2];
+		student.year = atoi(studentList[4*i + 3]);
+		print_student(format, student);
+  }
   return 0;
 }
